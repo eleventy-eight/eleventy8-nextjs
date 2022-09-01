16ac230b91d73df4752852e4f67eab08daf6c75a
Paul George <paul@thepropertyjungle.com>
2022-09-01 11:57:02 +0100

    Added GA script

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import { useEffect } from "react";
+import Script from "next/script";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }) {
+    // Import Bootstrap's core JS
     useEffect(() => {
         import("bootstrap/dist/js/bootstrap.js");
     }, []);
+
+    // Google Analytics
+    <>
+        <Script
+            strategy='afterInteractive'
+            src='https://www.googletagmanager.com/gtag/js?id=UA-168214894-1'
+        />
+        <Script strategy='afterInteractive'>
+            {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-168214894-1');
+                `}
+        </Script>
+    </>;
+
     return <Component {...pageProps} />;
 }
 
