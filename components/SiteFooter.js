0ceed2f9ffc6676a91d2c29760112a6c6885c2be
Paul George <paul@thepropertyjungle.com>
2022-09-02 12:38:07 +0100

    Removed offcanvas menu

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -5,7 +5,13 @@ export default function SiteFooter() {
     return (
         <footer>
             <p>
-                &copy; 2022 Eleventy8 | Built by Paul of Eleventy8 with{" "}
+                &copy; 2022 Eleventy8 | Built by{" "}
+                <Link href='https://www.linkedin.com/in/paul-g-4611521a2/'>
+                    <a rel='noopener noreferrer' target='_blank'>
+                        Paul
+                    </a>
+                </Link>{" "}
+                of Eleventy8 with{" "}
                 <Link href='https://nextjs.org/'>
                     <a rel='noopener noreferrer' target='_blank'>
                         NextJS
