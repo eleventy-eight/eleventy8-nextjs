0fe5382d18567c3003308f3161f5d00a8367893f
Paul George <paul@thepropertyjungle.com>
2022-09-24 09:37:02 +0100

    Added OneSignal to site

--- pages/_app.js
+++ pages/_app.js
@@ -18,19 +18,30 @@ function MyApp({ Component, pageProps }) {
         />
         <Script id='ga' strategy='afterInteractive'>
             {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'UA-168214894-1');
-                `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-168214894-1');
+            `}
         </Script>
         <Script
             strategy='afterInteractive'
             src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6140993113558726'
             crossorigin='anonymous'
         />
-        <Script id='aosPlugin' strategy='afterInteractive'>
-            AOS.init();
+        <Script
+            strategy='afterInteractive'
+            src='https://cdn.onesignal.com/sdks/OneSignalSDK.js'
+        />
+        <Script id='oneSignal' strategy='afterInteractive'>
+            {`
+            window.OneSignal = window.OneSignal || [];
+                OneSignal.push(function() {
+                OneSignal.init({
+                    appId: "364e5837-42bc-4c84-8bfa-74409932026d",
+                });
+            });
+            `}
         </Script>
     </>;
 
